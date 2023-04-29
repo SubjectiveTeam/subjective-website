e2e3dd18a05050c2f86eba0e395d45e9a237b1d3
Hugo <hugokorteapple@gmail.com>
2023-04-29 22:52:19 +0200

    Converted +page.server.ts to +page.ts where possible

---
+++ src/routes/clothing/+page.ts
@@ -0,0 +1,16 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async ({ parent }) => {
+    const { supabase } = await parent();
+
+    const { data, error } = await supabase
+    .from("products")
+    .select('*');
+
+    if (error) throw redirect(303, '/');
+    
+    return {
+        products: data
+    }
+}
