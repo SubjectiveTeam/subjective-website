576bc242041e73da77723a9ef338476b5141cd57
Hugo <hugokorteapple@gmail.com>
2023-05-08 23:50:28 +0200

    Removed store_view

--- src/routes/clothing/+page.ts
+++ src/routes/clothing/+page.ts
@@ -3,7 +3,20 @@ import { redirect } from '@sveltejs/kit';
 export async function load({ url, parent }) {
 	const { supabase } = await parent();
 
-	const { data, error } = await supabase.from('store_view').select('*');
+	// const { data, error } = await supabase.from('store_view').select('*');
+
+	const { data, error } = await supabase
+		.from('product_groups')
+		.select(
+			`
+	id, 
+	name,
+	description,
+	images,
+	products !inner ( * ) 
+	`
+		)
+		.is('products.active', true);
 
 	if (error) throw redirect(303, '/');
 
