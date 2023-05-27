10a90491acff3e732bbb76ce822e590de67c0d53
Hugo <hugokorteapple@gmail.com>
2023-05-27 14:57:27 +0200

    Added better padding for main

--- src/lib/components/layout/Main.svelte
+++ src/lib/components/layout/Main.svelte
@@ -5,7 +5,7 @@
 
 <div
 	class="
-		pt-[2.5vh] px-[5vw] md:px-[12.5vw] min-h-[calc(100vh-var(--header-height))]
+		pt-[2.5vh] pb-[7.5vh] px-[5vw] md:px-[12.5vw] min-h-[calc(100vh-var(--header-height))]
 		{$page.url.pathname === '/' ? 'xl:bg-[url("/homepage-bg.svg")] bg-cover bg-center' : ''}
 		">
 	<BreadCrumbs />
